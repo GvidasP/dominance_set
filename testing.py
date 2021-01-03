7e9f8aee6f11040a3c8e78cce54aca96273ef7f1
gvidasp <gvidaspran@gmail.com>
2021-01-03 23:26:44 +0200

    removed unnesesary code

--- testing.py
+++ testing.py
@@ -6,28 +6,6 @@ from bishops import Bishops
 from queens import Queens
 
 
-# def find_bishops(size):
-#     results = []
-
-#     bishops = Bishops(size)
-#     for i in range(100):
-#         result = bishops.solve()
-#         results.append(result)
-
-#     return results
-
-
-# def find_queens(size):
-#     results = []
-
-#     queens = Queens(size)
-#     for i in range(100):
-#         result = queens.solve()
-#         results.append(result)
-
-#     return results
-
-
 def find_queens(n_from, n_to):
     avg = []
     for j in tqdm(range(n_from, n_to + 1), desc='Valdoves'):
