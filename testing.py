66110efc3fa35817a3f75a5c0c72a5ba4813ecfb
gvidasp <gvidaspran@gmail.com>
2021-01-03 23:26:13 +0200

    updated testing

--- testing.py
+++ testing.py
@@ -1,79 +1,64 @@
 import concurrent.futures
-import threading
-import statistics
 from tqdm import tqdm
-from queue import Queue
+import statistics
 
-from queens import Queens
 from bishops import Bishops
+from queens import Queens
 
 
-def start_queens_process(size):
-    queens_class = Queens(size)
+# def find_bishops(size):
+#     results = []
 
-    results = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-        futures = [executor.submit(
-            queens_class.solve) for i in range(100)]
+#     bishops = Bishops(size)
+#     for i in range(100):
+#         result = bishops.solve()
+#         results.append(result)
 
-        for future in concurrent.futures.as_completed(futures):
-            results.append(future.result())
-
-    return results, size
+#     return results
 
 
-def start_bishops_process(size):
-    bishops_class = Bishops(size)
+# def find_queens(size):
+#     results = []
 
-    results = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-        futures = [executor.submit(
-            bishops_class.solve) for i in range(100)]
+#     queens = Queens(size)
+#     for i in range(100):
+#         result = queens.solve()
+#         results.append(result)
 
-        for future in concurrent.futures.as_completed(futures):
-            results.append(future.result())
-
-    return results, size
+#     return results
 
 
-def t_queens(n_to, n_from):
-    with tqdm(total=((n_to - n_from + 1)), desc='Valdoves') as pbar:
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            mean_queens = {}
-            queens_futures = [executor.submit(
-                start_queens_process, i) for i in range(n_from, n_to + 1)]
+def find_queens(n_from, n_to):
+    avg = []
+    for j in tqdm(range(n_from, n_to + 1), desc='Valdoves'):
+        queens = Queens(j)
+        results = []
+        for i in range(100):
+            result = queens.solve()
+            results.append(result)
 
-            for future in concurrent.futures.as_completed(queens_futures):
-                result_queens, size = future.result()
-                queens_avg = statistics.mean(result_queens)
-                mean_queens[size] = queens_avg
-                pbar.update(1)
+        avg.append(statistics.mean(results))
 
-    return 'Valdoviu sprendimo laiko vidurkis didejimo tvarka (n: laikas): {}'.format(
-        dict(sorted(mean_queens.items(), key=lambda item: item[1])))
+    return avg
 
 
-def t_bishops(n_to, n_from):
-    with tqdm(total=((n_to - n_from + 1)), desc='Rikiai') as pbar:
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            mean_bishops = {}
-            bishops_futures = [executor.submit(
-                start_bishops_process, i) for i in range(n_from, n_to + 1)]
+def find_bishops(n_from, n_to):
+    avg = []
+    for j in tqdm(range(n_from, n_to + 1), desc='Rikiai'):
+        bishops = Bishops(j)
+        results = []
+        for i in range(100):
+            result = bishops.solve()
+            results.append(result)
 
-            for future in concurrent.futures.as_completed(bishops_futures):
-                result_bishops, size = future.result()
-                bishops_avg = statistics.mean(result_bishops)
-                mean_bishops[size] = bishops_avg
-                pbar.update(1)
+        avg.append(statistics.mean(results))
 
-    return 'Rikiu sprendimo laiko vidurkis didejimo tvarka (n: laikas): {}'.format(
-        dict(sorted(mean_bishops.items(), key=lambda item: item[1])))
+    return avg
 
 
 def do_testing(n_from, n_to):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        queens = executor.submit(t_queens, n_to, n_from)
-        bishops = executor.submit(t_bishops, n_to, n_from)
+    queens_res = find_queens(n_from, n_to)
+    bishops_res = find_bishops(n_from, n_to)
 
-        print(queens.result())
-        print(bishops.result())
+    print(queens_res)
+    print(bishops_res)
